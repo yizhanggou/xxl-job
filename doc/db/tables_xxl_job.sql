1e46a70fe054c66a9ccbd4426e1414dcf37accb5
yizhanggou <leon69agong@163.com>
2019-12-31 16:55:27 +0800

    add test

--- doc/db/tables_xxl_job.sql
+++ doc/db/tables_xxl_job.sql
@@ -1,7 +1,7 @@
 #
 # XXL-JOB v2.2.0-SNAPSHOT
 # Copyright (c) 2015-present, xuxueli.
-
+#
 CREATE database if NOT EXISTS `xxl_job` default character set utf8mb4 collate utf8mb4_unicode_ci;
 use `xxl_job`;
 
